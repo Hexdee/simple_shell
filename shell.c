a0bb4c2de9c4b7b218ba9b6eade0d8743a0d6686
hexdee <lukmansaheed79@gmail.com>
2022-05-19 15:40:15 +0000

    Fix betty errors

--- shell.c
+++ shell.c
@@ -20,10 +20,11 @@ int main(__attribute__((unused))int argc, char **argv)
 
 	while (1)
 	{
-		write(STDIN_FILENO, "$ ", 3);
+		write(STDIN_FILENO, "$ ", 2);
 		lineLen = getline(&line, &len, stdin);
 		if (lineLen == -1)
 		{
+			write(STDIN_FILENO, "\n", 1);
 			free(line);
 			exit(EXIT_SUCCESS);
 		}
@@ -32,7 +33,7 @@ int main(__attribute__((unused))int argc, char **argv)
 		if (execute(args))
 			perror(argv[0]);
 
-		if (isatty(STDIN_FILENO))
+		if (!isatty(STDIN_FILENO))
 			break;
 	}
 	return (0);
