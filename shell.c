ab42db574539f7e1c5c36bbbb5c261f16af7cf5e
hexdee <lukmansaheed79@gmail.com>
2022-05-19 14:19:50 +0000

    Allow non-interactive mode

--- shell.c
+++ shell.c
@@ -19,18 +19,20 @@ int main(__attribute__((unused))int argc, char **argv)
 	size_t len = 0;
 
 	while (1)
-	{
+	{ 
 		write(STDIN_FILENO, "$ ", 3);
 		cmdLen = getline(&cmd, &len, stdin);
 		if (cmdLen == -1)
 		{
-			printf("\n");
 			free(cmd);
 			exit(EXIT_SUCCESS);
 		}
 		cmd[cmdLen - 1] = '\0';
 		if (execute(cmd))
 			perror(argv[0]);
+
+		if (isatty(STDIN_FILENO))
+			break;
 	}
 	return (0);
 }
